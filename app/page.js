e42f5feb2f3cc2900b33969763e641e72f1b8b85
Yuvadi29 <adityatrivedi612@gmail.com>
2024-01-02 12:57:34 +0530

    Added the onScroll Direction Change

--- app/page.js
+++ app/page.js
@@ -4,21 +4,38 @@ import Image from 'next/image'
 import React, { useEffect, useRef } from 'react'
 import styles from './page.module.css';
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 
 const page = () => {
   const firstText = useRef(null);
   const secondText = useRef(null);
+  const slider = useRef(null);
   let xPercent = 0;
   let direction = -1;
 
   useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
     requestAnimationFrame(animation);
+
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: 0,
+        end: window.innerHeight,
+        scrub: 0.25,
+        onUpdate: e => direction = e.direction * -1,
+      },
+      x: "-300px",
+    })
   }, []);
 
   const animation = () => {
     if (xPercent <= -100) {
       xPercent = 0;
+    }
+    if (xPercent > 0) {
+      xPercent = -100;
     }
     gsap.set(firstText.current, { xPercent: xPercent })
     gsap.set(secondText.current, { xPercent: xPercent })
@@ -36,7 +53,7 @@ const page = () => {
       />
       <div className="absolute top-[calc(100vh_-_350px)]">
         <div className="relative whitespace-nowrap">
-          <div className={styles.slider}>
+          <div ref={slider} className={styles.slider}>
             <p
               ref={firstText}
               className='relative m-0 text-white text-[230px] font-medium pr-[50px]'>Content Creator -</p>
