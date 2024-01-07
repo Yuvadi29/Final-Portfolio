6ad08f60d8865763e1c0a7213ebbd9be033dc707
Yuvadi29 <adityatrivedi612@gmail.com>
2024-01-07 16:53:26 +0530

    Modal Component Added

--- app/components/Modal/Modal.jsx
+++ app/components/Modal/Modal.jsx
@@ -1,27 +1,30 @@
 import Image from 'next/image';
-import React from 'react';
-import styles from './styles.module.css';
 
-const Modal = ({ modal, Work }) => {
+const Modal = ({ modal, projects }) => {
+
+  const { active, index } = modal;
+
   return (
-    <div className={styles.ModalContainer}>
-      <div className={styles.modalSlider}>
-        {Work?.work.map((project, index) => {
-          const { src, color } = project;
-          return (
-            <div key={`modal_${index}`} style={{ backgroundColor: color }} className={styles.modal}>
-              <Image
-                src={`/images/${src}`}
-                width={300}
-                height={0}
-                alt='image'
-              />
-            </div>
-          )
-        })}
+    <>
+      <div className=" h-[350px] w-[400px] absolute flex justify-center items-center overflow-hidden">
+        <div className="h-[100%] w-[100%] absolute transition-all duration-500 ease-in-out cubic-bezier(0.76,0,0.24,1)" style={{ top: index * -100 + "%" }}>
+          {
+            projects.map((project, index) => {
+              const { src, color } = project
+              return <div className="h-[100%] w-[100%] flex justify-center items-center relative" style={{ backgroundColor: color }} key={`modal_${index}`}>
+                <Image
+                  src={`/images/${src}`}
+                  width={300}
+                  height={0}
+                  alt="image"
+                />
+              </div>
+            })
+          }
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
-export default Modal
+export default Modal;
