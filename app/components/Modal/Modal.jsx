29fd09b784c6037406eee6bf8c47d1b3bb9c4eeb
Yuvadi29 <adityatrivedi612@gmail.com>
2024-01-07 16:59:51 +0530

    Enter and Exit of Modal using Framer Motion

--- app/components/Modal/Modal.jsx
+++ app/components/Modal/Modal.jsx
@@ -1,4 +1,13 @@
+import { motion } from 'framer-motion';
 import Image from 'next/image';
+
+
+const ScaleAnimation = {
+  initial: { scale: 0, x: "-50%", y: "-50%" },
+  open: { scale: 1, x: "-50%", y: "-50%", transition: { duration: 0.4, ease: [0.76, 0, 0.24, 1] } },
+  closed: { scale: 0, x: "-50%", y: "-50%", transition: { duration: 0.4, ease: [0.32, 0, 0.67, 0] } },
+}
+
 
 const Modal = ({ modal, projects }) => {
 
@@ -6,7 +15,7 @@ const Modal = ({ modal, projects }) => {
 
   return (
     <>
-      <div className=" h-[350px] w-[400px] absolute flex justify-center items-center overflow-hidden">
+      <motion.div variants={ScaleAnimation} initial={"initial"} animate={active ? "open" : "closed"} className=" h-[350px] w-[400px] absolute flex justify-center items-center overflow-hidden">
         <div className="h-[100%] w-[100%] absolute transition-all duration-500 ease-in-out cubic-bezier(0.76,0,0.24,1)" style={{ top: index * -100 + "%" }}>
           {
             projects.map((project, index) => {
@@ -22,7 +31,7 @@ const Modal = ({ modal, projects }) => {
             })
           }
         </div>
-      </div>
+      </motion.div >
     </>
   )
 }
